aed1eabb3cb3e6160fc0e703f706f02af6c7b60a
Seal <info@sealsecurity.io>
2026-06-18 20:08:36 +0000

    patched 3.7.1

--- clients/src/main/java/org/apache/kafka/common/requests/SaslAuthenticateRequest.java
+++ clients/src/main/java/org/apache/kafka/common/requests/SaslAuthenticateRequest.java
@@ -78,5 +78,13 @@ public class SaslAuthenticateRequest extends AbstractRequest {
         return new SaslAuthenticateRequest(new SaslAuthenticateRequestData(new ByteBufferAccessor(buffer), version),
             version);
     }
+
+    // Do not print authBytes, overwrite a temp copy of the data with empty bytes
+    @Override
+    public String toString() {
+        SaslAuthenticateRequestData tempData = data.duplicate();
+        tempData.setAuthBytes(new byte[0]);
+        return tempData.toString();
+    }
 }
 
